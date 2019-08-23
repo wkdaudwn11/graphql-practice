66a8dc8946884bfe273db5ef34d2e32ed9f8eca7
JangMyungJoo <wkdaudwn11@naver.com>
2019-08-23 22:24:54 +0900

    Getting data from graphgq api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { ApolloProvider } from 'react-apollo';
+import { HashRouter as Router, Route } from "react-router-dom";
 import client from './apolloClient';
+
+import Home from './Home';
+import Detail from './Detail';
 
 class App extends Component {
     render() {
         return (
             <ApolloProvider client={client}>
                 <div className="App" />
+                <Router>
+                    <>
+                        <Route exact={true} path={"/"} componen={Home} />
+                        <Route path={"/details/:movieId"} componen={Detail} />
+                    </>
+                </Router>
             </ApolloProvider>
         );
     }
--- src/apolloClient.js
+++ src/apolloClient.js
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost';
 
 // 요청 할 URL 정의
 const client = new ApolloClient({
-    uri: "https://movieql-osezlvyqsg.now.sh"
+    uri: "https://movieql.now.sh/"
 });
 
 export default client;
